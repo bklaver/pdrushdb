22fcb4e60e74cccb3d917cdd5bb6bcbdade1c3cb
Brad Klaver <bklaver@echidna.ca>
2017-05-08 13:28:05 -0400

    experiments

--- .pdrushdb.sh
+++ .pdrushdb.sh
@@ -13,6 +13,13 @@ function pdrushdb() {
   # Gets the current directory name. This corresponds to pantheons site alias.
   pantheon_site_alias=${PWD##*/}
 
+  local DRUPAL_VERSION="$(drush php-eval 'echo drush_drupal_major_version();')"
+  if [ ! $DRUPAL_VERSION ]; then
+    echo "No Drupal installation found."
+    echo "Run this command from the root of a Drupal site that is hosted on Pantheon"
+    return 1
+  fi
+  
   if [ -z "$1" ]; then
     environment="live"
     # if no argument is specified, we assume we'll get the live database.
@@ -21,20 +28,153 @@ function pdrushdb() {
     environment=$1
   fi
   
-  echo "Creating a backup on the ${environment} environment."
-  echo "terminus backup:create $pantheon_site_alias.$environment --element=db"
-  terminus backup:create $pantheon_site_alias.$environment --element=db
-  echo "Download backup as $pantheon_site_alias.sql.gz"
-  echo "terminus backup:get $pantheon_site_alias.$environment --element=db --to=./$pantheon_site_alias-$environment.sql.gz"
-  terminus backup:get $pantheon_site_alias.$environment --element=db --to=./$pantheon_site_alias-$environment.sql.gz
+  timestamp=`date '+%b%d_%I-%M%p'`
   
-  if file "${pantheon_site_alias}-${environment}.sql.gz" | grep -q "gzip compressed data"; then
-    echo "Gunzip and importing..."
-    gunzip -c $pantheon_site_alias-$environment.sql.gz | drush sql-cli
+  echo "Creating a backup on the ${environment} environment."
+  
+  # Turn on printing commands for learning.
+  set -x
+  
+  terminus backup:create $pantheon_site_alias.$environment --element=db
+  # Actually download the database.
+  terminus backup:get $pantheon_site_alias.$environment --element=db --to=./$pantheon_site_alias-$environment-$timestamp.sql.gz
+  
+  # Turn off the printing of commands.
+  { set +x; } 2>/dev/null
+  
+  if file "${pantheon_site_alias}-${environment}-$timestamp.sql.gz" | grep -q "gzip compressed data"; then
+    echo "G G G G G-UNZIP"
+    echo "gunzip -c $pantheon_site_alias-$environment-$timestamp.sql.gz | drush sql-cli"
+    gunzip -c $pantheon_site_alias-$environment-$timestamp.sql.gz | drush sql-cli
   else
     # I don't think this ever happens.
     echo "Importing..."
-    drush sql-cli $pantheon_site_alias.$environment
+    drush sql-cli $pantheon_site_alias.$environment-$timestamp.sql
   fi
 
+
 }
+
+
+
+
+function tdeploy() {  
+  # This works if:
+  # -you have terminus and drush installed
+  # -when you ran git clone, you didn't specify a directory name
+  # and used the default directory name.
+  
+  # Usage:
+  # cd into the root drupal directory. If you don't specify an arugment it gets the live database.
+  # pdrushdb [dev|test]
+  # E.g.) drushdb test
+  
+  # Gets the current directory name. This corresponds to pantheons site alias.
+  pantheon_site_alias=${PWD##*/}
+
+  local DRUPAL_VERSION="$(drush php-eval 'echo drush_drupal_major_version();')"
+  if [ ! $DRUPAL_VERSION ]; then
+    echo "No Drupal installation found."
+    echo "Run this command from the root of a Drupal site that is hosted on Pantheon"
+    return 1
+  fi
+  
+  if [ -z "$1" ]; then
+    echo "Specify the environment to deploy to with the first argument.  e.g.) tdeploy test"  
+  else
+    environment=$1
+  fi
+  set -x
+  terminus env:deploy $pantheon_site_alias.$environment --note "$2"
+  # Turn off the printing of commands.
+  { set +x; } 2>/dev/null
+}
+
+
+function tcc() {  
+  # This works if:
+  # -you have terminus and drush installed
+  # -when you ran git clone, you didn't specify a directory name
+  # and used the default directory name.
+  
+  # Usage:
+  # cd into the root drupal directory. If you don't specify an arugment it gets the live database.
+  # pdrushdb [dev|test]
+  # E.g.) drushdb test
+  
+  # Gets the current directory name. This corresponds to pantheons site alias.
+  pantheon_site_alias=${PWD##*/}
+
+  local DRUPAL_VERSION="$(drush php-eval 'echo drush_drupal_major_version();')"
+  if [ ! $DRUPAL_VERSION ]; then
+    echo "No Drupal installation found."
+    echo "Run this command from the root of a Drupal site that is hosted on Pantheon"
+    return 1
+  fi
+  
+  if [ -z "$1" ]; then
+    echo "Specify the environment to deploy to with the first argument.  e.g.) tdeploy test"  
+  else
+    environment=$1
+  fi
+  set -x
+  terminus env:clear-cache $pantheon_site_alias.$environment
+  # Turn off the printing of commands.
+  { set +x; } 2>/dev/null
+}
+
+
+
+
+
+function agopantheondb() {  
+  # This works if:
+  # -you have terminus and drush installed
+  # -when you ran git clone, you didn't specify a directory name
+  # and used the default directory name.
+  
+  # Usage:
+  # cd into the root drupal directory. If you don't specify an arugment it gets the live database.
+  # pdrushdb [dev|test]
+  # E.g.) drushdb test
+  
+  # Gets the current directory name. This corresponds to pantheons site alias.
+  pantheon_site_alias=ago
+
+  local DRUPAL_VERSION="$(drush php-eval 'echo drush_drupal_major_version();')"
+  if [ ! $DRUPAL_VERSION ]; then
+    echo "No Drupal installation found."
+    echo "Run this command from the root of a Drupal site that is hosted on Pantheon"
+    return 1
+  fi
+  
+  environment=test
+  
+  timestamp=`date '+%b%d_%I-%M%p'`
+  
+  echo "Creating a backup on the ${environment} environment."
+  
+  # Turn on printing commands for learning.
+  set -x
+  
+  terminus backup:create $pantheon_site_alias.$environment --element=db
+  # Actually download the database.
+  terminus backup:get $pantheon_site_alias.$environment --element=db --to=./$pantheon_site_alias-$environment-$timestamp.sql.gz
+  
+  # Turn off the printing of commands.
+  { set +x; } 2>/dev/null
+  
+  if file "${pantheon_site_alias}-${environment}-$timestamp.sql.gz" | grep -q "gzip compressed data"; then
+    echo "G G G G G-UNZIP"
+    echo "gunzip -c $pantheon_site_alias-$environment-$timestamp.sql.gz | drush sql-cli"
+    gunzip -c $pantheon_site_alias-$environment-$timestamp.sql.gz | drush sql-cli
+  else
+    # I don't think this ever happens.
+    echo "Importing..."
+    drush sql-cli $pantheon_site_alias.$environment-$timestamp.sql
+  fi
+
+
+}
+
+
